d7c98e694c8be03ae0e2d0bea299d544d7ae12f9
Matt Volonnino <mvolonnino12@gmail.com>
2020-09-21 21:38:05 -0400

    updated api call to pull 500 for better user experience

--- src/utils/API.js
+++ src/utils/API.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
   getEmployees: () => {
-    return axios.get(`https://randomuser.me/api/?results=10`);
+    return axios.get(`https://randomuser.me/api/?results=500`);
   },
 };
